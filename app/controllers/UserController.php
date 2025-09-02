1b4da174c4a4f12f730cc248ebdc3e0dd6aff46d
asdwdev <asdwdev@gmail.com>
2025-09-02 14:21:59 +0700

    handle parameter url (hardcore)

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -17,4 +17,27 @@ class UserController
         }
         echo "</ul>";
     }
+
+    public function show($id)
+    {
+        $userModel = new User();
+        $users = $userModel->getAllUsers();
+
+        // cari user berdasarkan ID
+        $user = null;
+        foreach ($users as $u) {
+            if ($u['id'] == $id) {
+                $user = $u;
+                break;
+            }
+        }
+
+        if ($user) {
+            echo "<h1>Detail User</h1>";
+            echo "ID: {$user['id']} <br>";
+            echo "Name: {$user['name']}";
+        } else {
+            echo "User dengan ID {$id} tidak ditemukan.";
+        }
+    }
 }
--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 
-// ambil URL path
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // daftar route
@@ -13,11 +12,21 @@ $routes = [
     "/users" => [UserController::class, "index"],
 ];
 
-// cek apakah route ada
+// cek static routes dulu
 if (array_key_exists($path, $routes)) {
     [$controller, $method] = $routes[$path];
     (new $controller())->$method();
-} else {
-    http_response_code(404);
-    echo "404 Not Found";
+    exit;
 }
+
+// cek dynamic route: /users/{id}
+if (preg_match("#^/users/(\d+)$#", $path, $matches)) {
+    $id = $matches[1]; // tangkap angka dari URL
+    $controller = new UserController();
+    $controller->show($id);
+    exit;
+}
+
+// kalau gak ada, 404
+http_response_code(404);
+echo "404 Not Found";
